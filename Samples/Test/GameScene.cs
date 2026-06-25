a8210c3d0fa2d246537db34cf403922d79676e94
eyilee <eyilee@protonmail.com>
2026-06-25 14:45:30 +0800

    add Test project

---
+++ Samples/Test/GameScene.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Library;
+using MonoGame.Library.Graphics;
+
+namespace Test;
+
+public class GameScene : Scene
+{
+    private Sprite? _slimeSprite;
+    private Sprite? _batSprite;
+    private Texture2DResource? _pixelTexture;
+    private Sprite? _pixelSprite;
+
+    public override void Initialize ()
+    {
+        base.Initialize ();
+    }
+
+    public override void LoadContent ()
+    {
+        TextureAtlas atlas = TextureAtlas.FromFile (Content, "atlas-definition.xml");
+
+        if (atlas.TryGetRegion ("slime", out TextureRegion? slimeRegion) && slimeRegion != null)
+        {
+            _slimeSprite = new Sprite (slimeRegion)
+            {
+                Position = new Vector2 (100, 100),
+            };
+        }
+
+        if (atlas.TryGetRegion ("bat", out TextureRegion? batRegion) && batRegion != null)
+        {
+            _batSprite = new Sprite (batRegion)
+            {
+                Position = new Vector2 (200, 100),
+            };
+        }
+
+        Texture2D texture = new (GraphicsDevice, 1, 1);
+        texture.SetData ([Color.White]);
+        _pixelTexture = new Texture2DResource ("pixel", texture);
+
+        _pixelSprite = new Sprite (new TextureRegion (_pixelTexture, new Rectangle (0, 0, 1, 1)))
+        {
+            Size = new Vector2 (2, 2),
+            Position = new Vector2 (100, 100),
+            Color = Color.White,
+        };
+
+        base.LoadContent ();
+    }
+
+    public override void UnloadContent ()
+    {
+        _pixelTexture?.Dispose ();
+
+        base.UnloadContent ();
+    }
+
+    protected override void Dispose (bool disposing)
+    {
+        if (disposing)
+        {
+        }
+
+        base.Dispose (disposing);
+    }
+
+    public override void Draw (GameTime gameTime)
+    {
+        GraphicsDevice.Clear (Color.CornflowerBlue);
+
+        _slimeSprite?.Draw (Render);
+        _batSprite?.Draw (Render);
+        _pixelSprite?.Draw (Render);
+
+        base.Draw (gameTime);
+    }
+}
